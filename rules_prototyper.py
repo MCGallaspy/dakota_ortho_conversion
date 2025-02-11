01fe19d14c616ef5174df49672af178e6715c12c
MCGallaspy <gallaspy.michael@gmail.com>
2025-02-11 13:30:55 -0800

    Ignore case

--- rules_prototyper.py
+++ rules_prototyper.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import unicodedata
+import re
 
 st.title("Orthography Conversion Rules Prototyper")
 
-num_rules = st.number_input("Number of rules", min_value=0, value=1)
 rules = []
+
+num_rules = st.number_input(
+    "Number of rules",
+    min_value=0,
+    value=1,
+    key="num_rules",
+)
 
 for i in range(num_rules):
     cols = st.columns(3)
@@ -16,17 +23,16 @@ for i in range(num_rules):
     if rule_type in ("Replace", "Replace (ignore accents)"):
         target = cols[1].text_input(
             "Replace this...",
-            key=f"rule_{i}_target",
+            key=f"rule_{i}_arg_0",
         )
         repl = cols[2].text_input(
             "...with this.",
-            key=f"rule_{i}_repl",
+            key=f"rule_{i}_arg_1",
         )
         rules.append((rule_type, target, repl))
 
 expander = st.expander("Export rules to CSV")
-for rule in rules:
-    expander.write(",".join(rule))
+expander.code("\n".join([",".join(rule) for rule in rules]))
 
 text_input = st.text_area(
     "Input",
@@ -41,7 +47,12 @@ for rule_type, *rule_args in rules:
         target, repl = rule_args
         target = unicodedata.normalize("NFC", target)
         repl = unicodedata.normalize("NFC", repl)
-        text_output = text_output.replace(target, repl)
+        text_output = re.sub(
+            target,
+            repl,
+            text_output,
+            flags=re.IGNORECASE,
+        )
     if rule_type == "Replace (ignore accents)":
         combining_accents = ("̀", "́")
         target, repl = rule_args
@@ -53,7 +64,12 @@ for rule_type, *rule_args in rules:
         target = unicodedata.normalize("NFC", target)
         repl = unicodedata.normalize("NFC", repl)
         text_output = unicodedata.normalize("NFD", text_output)                
-        text_output = text_output.replace(target, repl)
+        text_output = re.sub(
+            target,
+            repl,
+            text_output,
+            flags=re.IGNORECASE,
+        )
         text_output = unicodedata.normalize("NFC", text_output)
 
 st.header("Output")
